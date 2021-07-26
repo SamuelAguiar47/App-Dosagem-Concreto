1029a66684864fa9474cad5adc56b6ac229de296
Samuel <samuelsaguiar47@gmail.com>
2021-07-26 16:51:37 -0300

    Teste de Push depois da formatação do PC e reinstalação do Android Studio

--- app/src/main/java/com/cursoandroid/appdosagemconcreto/activities/MainActivity.java
+++ app/src/main/java/com/cursoandroid/appdosagemconcreto/activities/MainActivity.java
@@ -56,5 +56,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        buttonAjuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
