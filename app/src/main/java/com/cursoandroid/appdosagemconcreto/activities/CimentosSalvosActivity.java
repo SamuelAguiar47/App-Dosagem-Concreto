2c0129c4b208f1396acb5b57490f561400fe24fb
Samuel <samuelsaguiar47@gmail.com>
2021-08-13 11:40:39 -0300

    Configuração da exibição do recyclerViewCimentosSalvos

--- app/src/main/java/com/cursoandroid/appdosagemconcreto/activities/CimentosSalvosActivity.java
+++ app/src/main/java/com/cursoandroid/appdosagemconcreto/activities/CimentosSalvosActivity.java
@@ -1,6 +1,7 @@
 package com.cursoandroid.appdosagemconcreto.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -9,7 +10,8 @@ import android.view.View;
 import android.widget.Button;
 
 import com.cursoandroid.appdosagemconcreto.R;
-import com.cursoandroid.appdosagemconcreto.model.ItemTipoDeCimento;
+import com.cursoandroid.appdosagemconcreto.adapter.CimentoAdapter;
+import com.cursoandroid.appdosagemconcreto.model.ItemCimentoSalvo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,8 +20,9 @@ public class CimentosSalvosActivity extends AppCompatActivity {
 
     private Button buttonAdicionarCimento;
     private RecyclerView recyclerViewCimentosSalvos;
-    private ItemTipoDeCimento itemTipoDeCimento = new ItemTipoDeCimento();
-    private List<ItemTipoDeCimento> listaDeCimentos = new ArrayList<>();
+    private CimentoAdapter cimentoAdapter;
+    private ItemCimentoSalvo itemTipoDeCimento = new ItemCimentoSalvo();
+    private List<ItemCimentoSalvo> listaDeCimentos = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,19 +46,31 @@ public class CimentosSalvosActivity extends AppCompatActivity {
     public void carregarListaDeCimentos() {
 
         //Listar cimentos
-        ItemTipoDeCimento cimento1 = new ItemTipoDeCimento();
+        ItemCimentoSalvo cimento1 = new ItemCimentoSalvo();
         cimento1.setNomeDoCimento("Cimento 1");
         cimento1.setTempoDeCura("28");
         cimento1.setQtdeDePontos(36);
         listaDeCimentos.add(cimento1);
+
+        ItemCimentoSalvo cimento2 = new ItemCimentoSalvo();
+        cimento2.setNomeDoCimento("Cimento 2");
+        cimento2.setTempoDeCura("28");
+        cimento2.setQtdeDePontos(237);
+        listaDeCimentos.add(cimento2);
 
         /*
             Exibe pontos do cimento no RecyclerView
          */
 
         //Configurar um adapter
+        cimentoAdapter = new CimentoAdapter((ArrayList<ItemCimentoSalvo>) listaDeCimentos);
 
         //Configurar um RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerViewCimentosSalvos.setLayoutManager(layoutManager);
+        recyclerViewCimentosSalvos.setHasFixedSize(true);
+        recyclerViewCimentosSalvos.setAdapter(cimentoAdapter);
+
 
     }
 
--- app/src/main/java/com/cursoandroid/appdosagemconcreto/adapter/CimentoAdapter.java
+++ app/src/main/java/com/cursoandroid/appdosagemconcreto/adapter/CimentoAdapter.java
@@ -1,4 +1,66 @@
 package com.cursoandroid.appdosagemconcreto.adapter;
 
-public class CimentoAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cursoandroid.appdosagemconcreto.R;
+import com.cursoandroid.appdosagemconcreto.model.ItemCimentoSalvo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CimentoAdapter extends RecyclerView.Adapter<CimentoAdapter.MyViewHolder>{
+
+    private List<ItemCimentoSalvo> listaDeCimentosSalvos;
+
+    public CimentoAdapter(ArrayList<ItemCimentoSalvo> lista) {
+
+        this.listaDeCimentosSalvos = lista;
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemLista = LayoutInflater.from(parent.getContext())
+                                        .inflate(R.layout.lista_cimentos_salvos_adapter, parent, false);
+
+        return new MyViewHolder(itemLista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        ItemCimentoSalvo itemCimentoSalvo = listaDeCimentosSalvos.get(position);
+        holder.nomeDoCimento.setText(itemCimentoSalvo.getNomeDoCimento());
+        holder.tempoDeCura.setText("Cura: " + itemCimentoSalvo.getTempoDeCura() + " dias");
+        holder.qtdeDePontos.setText(itemCimentoSalvo.getQtdeDePontos() + " amostras");
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.listaDeCimentosSalvos.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nomeDoCimento, tempoDeCura, data, qtdeDePontos;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nomeDoCimento = itemView.findViewById(R.id.textViewNomeDoCimento);
+            tempoDeCura = itemView.findViewById(R.id.textViewTempoDeCura);
+            data = itemView.findViewById(R.id.textViewData);
+            qtdeDePontos = itemView.findViewById(R.id.textViewQtdeDePontos);
+
+        }
+    }
+
 }
