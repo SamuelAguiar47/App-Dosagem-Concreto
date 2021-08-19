04f0b2966fcbec9cd58cd9d7c04f03b9d0a00bc8
Samuel <samuelsaguiar47@gmail.com>
2021-08-19 09:56:29 -0300

    Início da configuração de DbCimentoCurva, ICurvaCimentoDAO e CurvaCimentoDAO, e configuração da opção de salvar para esse banco de dados.

--- app/src/main/java/com/cursoandroid/appdosagemconcreto/helper/CurvaCimentoDAO.java
+++ app/src/main/java/com/cursoandroid/appdosagemconcreto/helper/CurvaCimentoDAO.java
@@ -1,4 +1,67 @@
 package com.cursoandroid.appdosagemconcreto.helper;
 
-public class CurvaCimentoDAO {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.cursoandroid.appdosagemconcreto.model.ItemPontoCimento;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class CurvaCimentoDAO implements ICurvaCimentoDAO {
+
+    private SQLiteDatabase escreve;
+    private SQLiteDatabase le;
+
+    private String nomeDaTabela;
+
+    // Classes de arredondamento e formatação
+    private DecimalFormat arred0 = new DecimalFormat("####");
+    private DecimalFormat arred1X = new DecimalFormat("##0.#");
+    private DecimalFormat arred1 = new DecimalFormat("##0.0");
+    private DecimalFormat arred2 = new DecimalFormat("##0.00");
+    private DecimalFormat arred2x = new DecimalFormat("##0.##");
+    private DecimalFormat arred3 = new DecimalFormat("##0.000");
+    private DecimalFormat arred3x = new DecimalFormat("##0.###");
+
+    public CurvaCimentoDAO(Context context, String nomeDoCimento) {
+        nomeDaTabela = "tabela" + nomeDoCimento.replace(" ", "");
+        DbCimentoCurva dbCimentoCurva = new DbCimentoCurva(context, nomeDaTabela);
+        escreve = dbCimentoCurva.getWritableDatabase();
+        le = dbCimentoCurva.getReadableDatabase();
+    }
+
+    @Override
+    public boolean salvar(ItemPontoCimento itemPontoCimento) {
+
+        ContentValues cv = new ContentValues();
+        cv.put("ac", itemPontoCimento.getValorDeAC());
+        cv.put("fcj", itemPontoCimento.getValorDeFcj());
+
+        try {
+            escreve.insert(nomeDaTabela, null, cv);
+            Log.i("INFO", "Sucesso ao salvar ponto do cimento na tabela definitva.");
+        } catch (Exception e) {
+            Log.i("INFO", "Erro ao salvar ponto do cimento na tabela definitva.");
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean atualizar(ItemPontoCimento itemPontoCimento) {
+        return false;
+    }
+
+    @Override
+    public boolean deletar(ItemPontoCimento itemPontoCimento) {
+        return false;
+    }
+
+    @Override
+    public List<ItemPontoCimento> listar() {
+        return null;
+    }
 }
--- app/src/main/java/com/cursoandroid/appdosagemconcreto/helper/ICurvaCimentoDAO.java
+++ app/src/main/java/com/cursoandroid/appdosagemconcreto/helper/ICurvaCimentoDAO.java
@@ -1,4 +1,14 @@
 package com.cursoandroid.appdosagemconcreto.helper;
 
+import com.cursoandroid.appdosagemconcreto.model.ItemPontoCimento;
+
+import java.util.List;
+
 public interface ICurvaCimentoDAO {
+
+    public boolean salvar(ItemPontoCimento itemPontoCimento);
+    public boolean atualizar(ItemPontoCimento itemPontoCimento);
+    public boolean deletar(ItemPontoCimento itemPontoCimento);
+    public List<ItemPontoCimento> listar();
+
 }
