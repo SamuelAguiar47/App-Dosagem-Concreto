cd4f0be541cc244c60340714efc5b1c09ca32f8b
Samuel <samuelsaguiar47@gmail.com>
2021-08-19 10:10:42 -0300

    Configuração dos métodos de deletar e listar em CurvaCimentoDAO.

--- app/src/main/java/com/cursoandroid/appdosagemconcreto/helper/CurvaCimentoDAO.java
+++ app/src/main/java/com/cursoandroid/appdosagemconcreto/helper/CurvaCimentoDAO.java
@@ -2,12 +2,14 @@ package com.cursoandroid.appdosagemconcreto.helper;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
 import com.cursoandroid.appdosagemconcreto.model.ItemPontoCimento;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CurvaCimentoDAO implements ICurvaCimentoDAO {
@@ -57,11 +59,42 @@ public class CurvaCimentoDAO implements ICurvaCimentoDAO {
 
     @Override
     public boolean deletar(ItemPontoCimento itemPontoCimento) {
-        return false;
+
+        try {
+            String[] args = {itemPontoCimento.getId().toString()};
+            escreve.delete(nomeDaTabela, "id=?", args);
+            Log.i("INFO","Sucesso ao deletar ponto.");
+        } catch (Exception e) {
+            Log.i("INFO","Erro ao deletar ponto.");
+        }
+
+        return true;
     }
 
     @Override
     public List<ItemPontoCimento> listar() {
-        return null;
+
+        List<ItemPontoCimento> listaPontosCimento = new ArrayList<>();
+
+        String sql = "SELECT * FROM " + nomeDaTabela + " ;";
+        Cursor c = le.rawQuery(sql, null);
+
+        while ( c.moveToNext() ) {
+
+            ItemPontoCimento itemPontoCimento = new ItemPontoCimento();
+
+            Long id = c.getLong( c.getColumnIndex("id") );
+            Double valorDeAC = c.getDouble( c.getColumnIndex("ac") );
+            Double valorDeFcj = c.getDouble( c.getColumnIndex("fcj") );
+
+            itemPontoCimento.setId(id);
+            itemPontoCimento.setValorDeAC(valorDeAC);
+            itemPontoCimento.setValorDeFcj(valorDeFcj);
+            itemPontoCimento.setNomeDoPonto("a/c: " + arred2.format(valorDeAC) + "   ➔   Fcj: " + arred2x.format(valorDeFcj) + " MPa");
+
+            listaPontosCimento.add(itemPontoCimento);
+        }
+
+        return listaPontosCimento;
     }
 }
